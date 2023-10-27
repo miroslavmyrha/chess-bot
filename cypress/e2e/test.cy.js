baf234f899864ea0b668ff15eecbe40494155453
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-10-27 16:00:42 +0200

    feat(start game): start chess game with basic parameters

--- cypress/e2e/test.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Basic test', () => {
-  it('Visit chess.com page', () => {
-    cy.visit('https://chess.com')
-  })
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import languageMutationObject from './../fixtures/languageMutations.json'
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,42 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('startChessGame', (
+  chessGameParametersObject
+  ) => {
+    cy.visit('/')
+
+    cy.get('div')
+      .contains(
+        languageMutationObject[chessGameParametersObject.languageMutation].playOnline
+      )
+      .click()
+
+    cy.get('label[for="' + chessGameParametersObject.playerLevel + '"]')
+      .click()
+
+    cy.get('button')
+      .contains(
+        languageMutationObject[chessGameParametersObject.languageMutation].cookiesReject
+      )
+      .should('be.visible')
+      .click()
+
+    cy.get('button[type="submit"]')
+      .filter(':visible')
+      .click()
+
+    cy.get('button[type="button"]')
+      .contains(
+        languageMutationObject[chessGameParametersObject.languageMutation].playButton
+      )
+      .click()
+
+    cy.get('button[aria-label="Close"]')
+      .click()
+
+    cy.get('button[data-cy="new-game-index-play"]')
+      .click()
+})

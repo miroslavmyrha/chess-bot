b96b2d485bff79c9f5fc4757959e71b5383bafd5
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-10-28 00:40:10 +0200

    fix(first move): #4 increase default command timeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.chess.com',
+    defaultCommandTimeout: 100000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

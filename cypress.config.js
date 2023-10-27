d1858d6c459ab1217d95ae5f8fd49371bafc7670
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-10-27 22:35:47 +0200

    feat(first move): added first move test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://chess.com',
+    baseUrl: 'https://www.chess.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/playChess.cy.js
+++ cypress/e2e/playChess.cy.js
@@ -1,14 +1,31 @@
 import playerLevel from '../fixtures/playerLevel.json'
 
+// default 1000x660
+
+const selectedLanguageMutation = 'cz'
+
 describe('before: start chess game', () => {
   before(() => {
+    // x, y
+    cy.viewport(1000, 660)
     cy.startChessGame({
-      languageMutation: 'cz', 
+      languageMutation: selectedLanguageMutation, 
       playerLevel: playerLevel.new
     })
   })
 
-  it('Check first move', () => {
-    
+  it('Pawn´s first move test', () => {
+    // to-do: develop waiting for oponent connection method..
+    cy.wait(5000)
+    cy.getMyPlayerColor().then(myColor => {
+      if (myColor === 'white') {
+        cy.get('div.piece.wp.square-52').click()
+        cy.get('div.piece.wp.square-52').click(20, -90, {force: true})
+      } else {
+        // to-do: waiting for oponent move
+        cy.get('div.piece.bp.square-47').click()
+        cy.get('div.piece.bp.square-47').click(20, -90, {force: true})
+      }
+    })
   })
 })
